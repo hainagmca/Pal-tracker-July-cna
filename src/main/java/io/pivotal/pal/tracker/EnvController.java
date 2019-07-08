477a47026d864d20597e84567f1c1f84bf872efc
chundn4 <nagendra.chunduru@emc.com>
2019-07-08 16:29:20 +0530

    travis test

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -21,6 +21,7 @@ public class EnvController {
         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddress
     ) {
+
         this.port = port;
         this.memoryLimit = memoryLimit;
         this.cfInstanceIndex = cfInstanceIndex;
