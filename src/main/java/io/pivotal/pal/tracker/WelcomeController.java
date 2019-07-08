1038485cc7666b630a4143218ba3c9aa0d3093e6
chundn4 <nagendra.chunduru@emc.com>
2019-07-08 15:58:31 +0530

    gradlew updates

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -9,8 +9,7 @@ public class WelcomeController {
 
     private String welcomeMessage;
 
-    public WelcomeController(
-            @Value("${welcome.message}") String welcomeMessage
+    public WelcomeController(@Value("${welcome.message}") String welcomeMessage
     ) {
         this.welcomeMessage = welcomeMessage;
     }
